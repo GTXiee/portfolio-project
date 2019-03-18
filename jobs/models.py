0969ab44af312a121767465068c7427593834da4
George <georgethomaschristie@gmail.com>
2019-03-18 17:24:36 +0000

    A few changes

--- jobs/models.py
+++ jobs/models.py
@@ -5,10 +5,13 @@ from taggit.managers import TaggableManager
 
 class Job(models.Model):
     title = models.CharField(max_length=200, default='change me')
-    image = models.ImageField(upload_to='images/')
+    url = models.URLField(max_length=200, blank=True)
+    portfolio_image = models.ImageField(upload_to='images/', blank=True)
+    display_image = models.ImageField(upload_to='images/', blank=True)
     summary = models.CharField(max_length=200)
-    body = models.TextField()
-    tags = TaggableManager()
+    body = models.TextField(blank=True)
+    tags = TaggableManager(blank=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
